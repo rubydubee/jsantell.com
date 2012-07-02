67bbd4b822e3903e7aff8c6529651fec8706adf3
Jordan Santell <jsantell@gmail.com>
2012-07-02 08:15:03 -0400

    Init commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var
+  express  = require('express'),
+  swag     = require('swag-blog'),
+  app      = module.exports = express.createServer();
+
+swag( app, {
+  posts: './_posts/',
+  route: '/post/'
+});
+
+app.configure(function () {
+  app.set( 'views', __dirname + '/views' );
+  app.set( 'view engine', 'jade' );
+  app.use( express.static( __dirname + '/public' ));
+  app.use( app.router );
+});
+
+app.configure('development', function () {
+  app.use( express.errorHandler({ dumpExceptions: true, showStack: true }) );
+});
+
+app.configure( 'production', function () {
+  app.use( express.errorHandler() );
+});
+
+
+app.get('/', function ( req, res ) {
+  res.render('index');
+});
+
+
+app.listen( 3000 );
