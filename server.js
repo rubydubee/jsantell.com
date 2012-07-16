26dba42fefc218cc90c1195b4cb46cde1891a2d3
Jordan Santell <jsantell@gmail.com>
2012-07-15 21:40:02 -0400

    Remove routes from server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ swag.set({
   postsPerPage : 2,
   posts        : './_posts',
   metaFormat   : 'json'
-}).createPostRoute( '/post/', 'post' )
-  .createPostListRoute( '/posts/', 'postList' )
-  .createTagRoute( '/tag/', 'tag' )
-  .createCategoryRoute( '/category/', 'category' )
+}).createPostRoute( '/post/:post', 'post', '{TITLE}'
+  .createPostListRoute( '/posts/:page', 'postList' )
+  .createTagRoute( '/tag/:tag', 'tag' )
+  .createCategoryRoute( '/category/:category', 'category' )
   .init();
 
 
@@ -23,6 +23,7 @@ app.configure(function () {
   app.use( app.router );
 });
 
+
 app.configure('development', function () {
   app.use( express.errorHandler({ dumpExceptions: true, showStack: true }) );
 });
@@ -31,12 +32,6 @@ app.configure( 'production', function () {
   app.use( express.errorHandler() );
 });
 
-app.get( '/', function ( req, res ) {
-  res.render( 'index' );
-});
-
-app.get( '/projects', function ( req, res ) {
-  res.render( 'projects' );
-});
+require( './routes' )( app );
 
 app.listen( 3000 );
